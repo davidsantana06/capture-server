8ec5bcf8bfee9db6ef5eeb035bf302b302e2f72c
David Santana <davidsantana06@hotmail.com>
2024-10-14 13:37:21 -0300

    refactor(capture): resume message on print screen event

--- app/modules/capture/events.py
+++ app/modules/capture/events.py
@@ -1,5 +1,3 @@
-from flask import url_for
-
 from app.extensions import socket_io
 from app.facades import response
 
@@ -11,8 +9,7 @@ def on_print_screen(base64: str):
     file_name = capture_operations.generate_file_name()
     capture_operations.store(base64, file_name)
     capture = capture_operations.create(file_name)
-    next_url = url_for('capture.get_file_by_id', id=capture.id)
     return response.as_emission(
         'success',
-        f'The capture can be accessed at: {next_url}'
+        f'The capture was saved with identifier {capture.id}.'
     )
